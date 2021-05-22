194236ffecbd058be21fa5365cddd56c4cbb8cee
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2021-05-22 17:16:30 +0200

    Create nimbus-update.sh

---
+++ script/nimbus-update.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+echo ""
+
+echo "Updating Nimbus.."
+  
+if [ -f "/usr/local/bin/nimbus" ]; then
+
+  cd $HOME
+  git clone https://github.com/status-im/nimbus-eth2.git
+  cd nimbus-eth2
+  make -j4 nimbus_beacon_node
+  mv $HOME/nimbus-eth2/build/nimbus_beacon_node /usr/local/bin/nimbus
+  cd $HOME
+  rm -rf $HOME/nimbus-eth2
+  service nimbus restart
+  nimbus --version
+
+else
+
+  echo "Nimbus is not installed"
+fi

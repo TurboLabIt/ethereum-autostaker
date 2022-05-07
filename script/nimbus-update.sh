79d19cde65ae81aac644abdd6aeecdabdde5cec3
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2022-05-07 22:57:44 +0200

    Update nimbus-update.sh

--- script/nimbus-update.sh
+++ script/nimbus-update.sh
@@ -5,18 +5,18 @@ echo "Updating Nimbus.."
   
 if [ -f "/usr/local/bin/nimbus" ]; then
 
-  apt update
-  apt install build-essential git libpcre3-dev -y
+  sudo apt update
+  sudo apt install build-essential git libpcre3-dev -y
 
   cd $HOME
   git clone https://github.com/status-im/nimbus-eth2.git
   cd nimbus-eth2
   make -j4 nimbus_beacon_node
-  service nimbus stop
-  mv $HOME/nimbus-eth2/build/nimbus_beacon_node /usr/local/bin/nimbus
+  sudo service nimbus stop
+  sudo mv "$HOME/nimbus-eth2/build/nimbus_beacon_node" "/usr/local/bin/nimbus"
   cd $HOME
   rm -rf $HOME/nimbus-eth2
-  service nimbus restart
+  sudo service nimbus restart
   nimbus --version
 
 else
